68d48b9a3ea57786154076f3e7e13f00d9957eaa
Christopher Wray <christophertwray@gmail.com>
2025-09-22 20:02:29 -0700

    Update logo configuration to change type from 'relationship' to 'upload' for dark and light mode logos

--- web/src/Logo/config.ts
+++ web/src/Logo/config.ts
@@ -14,9 +14,8 @@ export const Logo: GlobalConfig = {
         description:
           'Add a logo that will be displayed when the site is rendered in dark mode, or when the header has an image.',
       },
-      type: 'relationship',
+      type: 'upload',
       relationTo: 'media',
-      hasMany: false,
     },
     {
       name: 'lightLogo',
@@ -24,9 +23,8 @@ export const Logo: GlobalConfig = {
         description:
           'Add a logo that will be displayed when your site is rendering in light mode, and in the header when no image is added.',
       },
-      type: 'relationship',
+      type: 'upload',
       relationTo: 'media',
-      hasMany: false,
     },
   ],
   hooks: {
